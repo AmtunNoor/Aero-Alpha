f102102bf26d53cc903d030073e564cfedbebc1f
AmtunNoor <prideofmariam+github@gmail.com>
2026-05-31 15:16:30 +0300

    Revise service worker cache and assets
    Updated cache version and added new asset images.

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,20 @@
-const CACHE = "arabic-flight-v1";
+const CACHE = "arabic-v13";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./game.js",
-  "./manifest.json"
+"./",
+"./index.html",
+"./game.js",
+"./manifest.json",
+"./assets/images/sky.webp",
+"./assets/images/airport.webp",
+"./assets/images/runway.webp",
+"./assets/images/plane.webp"
 ];
 
-// INSTALL
 self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE).then(c=>c.addAll(ASSETS))
-    );
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
-// FETCH
 self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(r=>r || fetch(e.request))
-    );
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
