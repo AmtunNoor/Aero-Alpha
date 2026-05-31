b421f8638f0d19cf6f6dc019a7d0533f68a773b9
AmtunNoor <prideofmariam+github@gmail.com>
2026-05-31 17:06:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,16 @@
-const CACHE = "arabic-v13";
+const CACHE = "arabic-v16";
 
 const ASSETS = [
 "./",
 "./index.html",
 "./game.js",
 "./manifest.json",
-"./assets/images/sky.webp",
+"./assets/images/sky_day.webp",
 "./assets/images/airport.webp",
 "./assets/images/runway.webp",
-"./assets/images/plane.webp"
+"./assets/images/plane.webp",
+"./assets/sound/engine.mp3",
+"./assets/sound/wind.mp3"
 ];
 
 self.addEventListener("install", e=>{
