cdebd8c1a79a0aad1de0df55f50144bcd502f515
AmtunNoor <prideofmariam+github@gmail.com>
2026-05-31 22:23:09 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,11 @@
-const CACHE = "arabic-v16";
-
-const ASSETS = [
-"./",
-"./index.html",
-"./game.js",
-"./manifest.json",
-"./assets/images/sky_day.webp",
-"./assets/images/airport.webp",
-"./assets/images/runway.webp",
-"./assets/images/plane_trainer.png",
-"./assets/sound/engine.mp3",
-"./assets/sound/wind.mp3"
-];
-
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
